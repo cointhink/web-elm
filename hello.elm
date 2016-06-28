d2141ce886704d8e2adc6df7122486fea3465b96
Don Park <donp@donp.org>
2016-06-27 19:49:29 -0700

    logo and text

--- hello.elm
+++ hello.elm
@@ -1,9 +1,14 @@
-import Html exposing (div, span, text)
+import Html exposing (div, span, text, img)
 import Html.App exposing (beginnerProgram)
 import Html.Attributes exposing (..)
 
 main = beginnerProgram{view = view, model = "", update = update}
 
-view model = div [ class "bob" ] [ text "hi" ]
+view model = div [ class "main" ]
+                 [
+                   div [] [ img [ class "biglogo", src "assets/logo-256.png" ] [] ],
+                   div [] [ text "Cointhink is being rebuilt." ],
+                   div [] [ text "Check back later." ]
+                 ]
 
 update msg model = ""
