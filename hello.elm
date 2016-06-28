48bf344e9a1b2514528994620e34192f7c655293
Don Park <donp@donp.org>
2016-06-27 20:33:01 -0700

    start the navbar

--- hello.elm
+++ hello.elm
@@ -4,9 +4,15 @@ import Html.Attributes exposing (..)
 
 main = beginnerProgram{view = view, model = "", update = update}
 
+navbar = div [ class "navbar" ]
+             [
+               span [] [ text "abc" ]
+             ]
+
 view model = div [ class "main" ]
                  [
-                   div [] [ img [ class "biglogo", src "assets/logo-256.png" ] [] ],
+                   navbar,
+                   div [] [ img [ class "biglogo", src "assets/logo.svg" ] [] ],
                    div [] [ text "Cointhink is being rebuilt." ],
                    div [] [ text "Check back later." ]
                  ]
