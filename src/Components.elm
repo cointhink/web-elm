573354ab2b0ef7667a852ffc0d8edfdf87d1aa2e
Don Park <donp@donp.org>
2016-07-11 18:59:59 -0700

    syntax spacing

--- src/Components.elm
+++ src/Components.elm
@@ -4,52 +4,63 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import String exposing (toUpper)
 
-navbar model = div [ class "navbar" ]
+navbar model =
+  div [ class "navbar" ]
+      [
+        span [ class "menuitem" ]
              [
-               span [ class "menuitem" ]
-                    [
-                      img [ class "littlelogo", src "assets/logo.svg" ] [],
-                      text "Cointhink"
-                    ],
-               span [ class "menuitem" ]
-                    [
-                      login
-                    ]
-
+               img [ class "littlelogo", src "assets/logo.svg" ] [],
+               text "Cointhink"
+             ],
+        span [ class "menuitem" ]
+             [
+               login
              ]
+      ]
 
-login = Html.form []
-                  [
-                    Html.input [
-                            type' "email",
-                            placeholder "username"
-                          ]
-                          [],
-                    button [ ] [ text "Signin" ]
-                  ]
+login =
+  Html.form []
+            [
+              Html.input [
+                      type' "email",
+                      placeholder "username"
+                    ]
+                    [],
+              button [ ] [ text "Signin" ]
+            ]
 
-chartZone model = div [ id "chartZone" ]
-                      [
-                        span [ class "pairtitle" ]
-                             [
-                                text (toUpper model.base),
-                                text "/",
-                                text (toUpper model.quote)
-                             ],
-                        chart
-                      ]
+chartZone model =
+  div [ id "chartZone" ]
+      [
+        span [ class "pairtitle" ]
+             [
+                text (toUpper model.base),
+                text "/",
+                text (toUpper model.quote)
+             ],
+        chart
+      ]
 
-chart = div [ id "chart" ] []
+chart =
+  div [ id "chart" ] []
 
-footer model = div [ class "footer" ]
-                   [ text model.ws_url ]
+centerBlock =
+  div [ id "centerblock" ]
+      [
+        div [] [ img [ class "biglogo", src "assets/logo.svg" ] [] ],
+        div [] [ text "Cointhink is being rebuilt." ],
+        div [] [ text "Check back later." ]
+      ]
 
-view model = div [ class "main" ]
-                 [
-                   navbar model,
-                   chartZone model,
-                   div [] [ img [ class "biglogo", src "assets/logo.svg" ] [] ],
-                   div [] [ text "Cointhink is being rebuilt." ],
-                   div [] [ text "Check back later." ],
-                   footer model
-                 ]
+footer model =
+  div [ class "footer" ]
+      [  ]
+
+view model =
+ div [ class "main" ]
+     [
+       navbar model,
+       chartZone model,
+       centerBlock,
+       footer model
+     ]
