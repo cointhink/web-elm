5fc49ecd45ff3a91a709f783d0be2dfb2c167f81
Don Park <donp@donp.org>
2016-07-29 10:36:45 -0700

    use the m= market url param

--- public/js/js.js
+++ public/js/js.js
@@ -229,3 +229,14 @@ function flatten(arr) {
     return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
   }, []);
 }
+
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
--- src/Cointhink.elm
+++ src/Cointhink.elm
@@ -98,11 +98,13 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
   WebSocket.listen model.ws_url ws_parse
 
-type alias Flags = { url : String }
+type alias Flags = { url : String,
+                     base : String,
+                     quote : String }
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    ( Model ((Debug.log "flags" flags).url) "USDT" "BTC" 4 [],
+    ( Model ((Debug.log "flags" flags).url) flags.base flags.quote 4 [],
       Cmd.batch [ setup (), send Cointhink.Shared.Init,
                             send Cointhink.Shared.ExchangesQuery ] )
 
