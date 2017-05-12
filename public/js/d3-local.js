28527cacf37fd030384834f2a70a1b0297c23490
Don Park <donp@donp.org>
2017-05-12 10:47:59 -0700

    ?token=

--- public/js/d3-local.js
+++ public/js/d3-local.js
@@ -262,12 +262,3 @@ function flatten(arr) {
 }
 
 
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
